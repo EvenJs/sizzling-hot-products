5c5021375411eaefdfd451f82ff413604eca165a
Johnson Wang <yuhang0229@gmail.com>
2026-05-16 20:53:02 +1000

    test: add unit tests for SizzlingHotService

---
+++ backend/tests/Application.Tests/SizzlingHotServiceTests.cs
@@ -0,0 +1,365 @@
+using FluentAssertions;
+
+using Application.Interfaces;
+using Application.Tests.Builders;
+
+using Domain;
+
+using NSubstitute;
+
+namespace Application.Tests;
+
+public class SizzlingHotServiceTests
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IProductRepository _productRepository;
+
+    public SizzlingHotServiceTests()
+    {
+        _orderRepository = Substitute.For<IOrderRepository>();
+        _productRepository = Substitute.For<IProductRepository>();
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_ReturnsProductWithMostSales()
+    {
+        // Arrange 
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("Measure").Build(),
+        };
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([new OrderEntry("P1", 1)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O2").WithEntries([new OrderEntry("P1",1)]).WithCustomerId("C2").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O3").WithEntries([new OrderEntry("P2",1)]).WithCustomerId("C3").WithDate(new DateOnly(2026, 4, 21)).Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetDailyTopProductsAsync(fromDate, toDate);
+
+        // Assert
+        var topProduct = result.First();
+        topProduct.Product.Name.Should().Be("Hammer");
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_CountsProductOncePerOrder_RegardlessOfQuantity()
+    {
+        // Arrange 
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("Measure").Build(),
+        };
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([new OrderEntry("P1", 5)]).WithCustomerId("C1").Build(),
+            new OrderBuilder().WithOrderId("O3").WithEntries([new OrderEntry("P2",1)]).WithCustomerId("C1").Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetDailyTopProductsAsync(fromDate, toDate);
+
+        // Assert
+        var topProduct = result.First();
+        topProduct.Product.Name.Should().Be("Hammer");
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_DeduplicatesSalesForSameCustomerOnSameDay()
+    {
+        // Arrange 
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("Measure").Build(),
+        };
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([new OrderEntry("P1", 1)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O2").WithEntries([new OrderEntry("P1",2)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O3").WithEntries([new OrderEntry("P2", 1)]).WithCustomerId("C2").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O4").WithEntries([new OrderEntry("P2", 1)]).WithCustomerId("C3").WithDate(new DateOnly(2026, 4, 21)).Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetDailyTopProductsAsync(fromDate, toDate);
+
+        // Assert
+        var topProduct = result.First();
+        topProduct.Product.Name.Should().Be("Measure");
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_ExcludesCancelledOrders()
+    {
+        // Arrange 
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("Measure").Build(),
+        };
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([new OrderEntry("P1", 1)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O2").WithEntries([new OrderEntry("P1",2)]).WithCustomerId("C2").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O1").WithStatus(OrderStatus.Cancelled).Build(),
+            new OrderBuilder().WithOrderId("O3").WithEntries([new OrderEntry("P2", 2)]).WithCustomerId("C3").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O4").WithEntries([new OrderEntry("P2", 1)]).WithCustomerId("C4").WithDate(new DateOnly(2026, 4, 21)).Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetDailyTopProductsAsync(fromDate, toDate);
+
+        // Assert
+        var topProduct = result.First();
+        topProduct.Product.Name.Should().Be("Measure");
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_SelectsAlphabeticallyFirstProduct_WhenSalesAreTied()
+    {
+        // Arrange 
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("BBQ").Build(),
+        };
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([new OrderEntry("P1", 5)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O2").WithEntries([new OrderEntry("P2",1)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetDailyTopProductsAsync(fromDate, toDate);
+
+        // Assert
+        var topProduct = result.First();
+        topProduct.Product.Name.Should().Be("BBQ");
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_IgnoresCancellation_WhenNoMatchingCompletedOrder()
+    {
+        // Arrange 
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("BBQ").Build(),
+        };
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([new OrderEntry("P1", 5)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O2").WithStatus(OrderStatus.Cancelled).Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetDailyTopProductsAsync(fromDate, toDate);
+
+        // Assert
+        var topProduct = result.First();
+        topProduct.Product.Name.Should().Be("Hammer");
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_OmitsDay_WhenAllOrdersForThatDayAreCancelled()
+    {
+        // Arrange 
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("BBQ").Build(),
+        };
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([new OrderEntry("P1", 5)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O1").WithStatus(OrderStatus.Cancelled).Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetDailyTopProductsAsync(fromDate, toDate);
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetPeriodTopProductAsync_ReturnsNull_WhenAllOrdersAreCancelled()
+    {
+        // Arrange 
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("BBQ").Build(),
+        };
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([new OrderEntry("P1", 5)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+            new OrderBuilder().WithOrderId("O1").WithStatus(OrderStatus.Cancelled).Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetPeriodTopProductAsync(fromDate, toDate);
+
+        // Result 
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_ReturnsEmpty_WhenOrdersListIsEmpty()
+    {
+        // Arrange
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("BBQ").Build(),
+        };
+
+        var orders = new List<Order>();
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetDailyTopProductsAsync(fromDate, toDate);
+
+        // Result 
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_ReturnsEmpty_WhenProductsListIsEmpty()
+    {
+        // Arrange
+        var products = new List<Product>();
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetDailyTopProductsAsync(fromDate, toDate);
+
+        // Result 
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetDailyTopProductsAsync_SkipsEntry_WhenProductIdNotFoundInProductsList()
+    {
+        // Arrange 
+        var products = new List<Product>
+        {
+            new ProductBuilder().WithId("P1").WithName("Hammer").Build(),
+            new ProductBuilder().WithId("P2").WithName("BBQ").Build(),
+        };
+
+        var orders = new List<Order>
+        {
+            new OrderBuilder().WithOrderId("O1").WithEntries([new OrderEntry("P99", 5)]).WithCustomerId("C1").WithDate(new DateOnly(2026, 4, 21)).Build(),
+        };
+
+        var fromDate = new DateOnly(2026, 4, 21);
+        var toDate = new DateOnly(2026, 4, 23);
+
+        _orderRepository.GetAllAsync().Returns(orders);
+        _productRepository.GetAllAsync().Returns(products);
+
+        var service = new SizzlingHotService(_orderRepository, _productRepository);
+
+        // Act
+        var result = await service.GetPeriodTopProductAsync(fromDate, toDate);
+
+        // Result 
+        result.Should().BeNull();
+    }
+}
